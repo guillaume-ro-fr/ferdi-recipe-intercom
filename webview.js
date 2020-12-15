92beb17b8a0f3529aeb51778e85c59bce0eda1ce
Guillaume RODRIGUEZ <guiguidu31300@gmail.com>
2020-12-15 22:43:59 +0100

    Change from Franz to Ferdi

--- webview.js
+++ webview.js
@@ -1,12 +1,12 @@
-module.exports = (Franz) => {
+module.exports = (Ferdi) => {
     function getMessages() {
       const numMessages = parseInt(document.querySelector('.left-nav [data-content="Inbox"] .unread__container .unread').innerHTML.trim());
       if (numMessages >= 0) {  
-        Franz.setBadge(numMessages, 0);
+        Ferdi.setBadge(numMessages, 0);
       } else {
-          Franz.setBadge(0, 0);
+         Ferdi.setBadge(0, 0);
       }
     }
   
-    Franz.loop(getMessages);
+    Ferdi.loop(getMessages);
   }
